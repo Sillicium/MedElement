2e1c57e10cadad5d18eebe196a59a842c3424187
Sillicium <102834057+Sillicium@users.noreply.github.com>
2024-06-27 13:14:43 +0500

    add styles in index page

--- MedElement/settings.py
+++ MedElement/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Добавьте эту строку, если у вас есть дополнительные пути для статических файлов
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
